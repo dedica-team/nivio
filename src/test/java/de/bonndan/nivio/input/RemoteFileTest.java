e360aed1962e304efac03b7d819e74117b5cb1e4
Daniel Pozzi <dpozzi@haeger-consulting.de>
2018-09-28 17:47:21 +0200

    added tests for header and basic auth

--- src/test/java/de/bonndan/nivio/input/RemoteFileTest.java
+++ src/test/java/de/bonndan/nivio/input/RemoteFileTest.java
@@ -2,6 +2,7 @@ package de.bonndan.nivio.input;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.matching.AnythingPattern;
 import de.bonndan.nivio.input.dto.SourceReference;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -46,6 +47,48 @@ public class RemoteFileTest {
 
         String serverUrl = buildApiUrl();
         SourceReference sourceReference = new SourceReference(serverUrl);
+        FileFetcher fetcher = new FileFetcher(new HttpService());
+        String s = fetcher.get(sourceReference);
+        assertEquals(yml, s);
+    }
+
+    @Test
+    void fetchesYamlWithHeader() {
+
+        String path = getRootPath() + "/src/test/resources/example/services/wordpress.yml";
+        String yml = FileFetcher.readFile(new File(path));
+
+        givenThat(
+                get("/some/file.yml")
+                        .withHeader("PRIVATE_KEY", new AnythingPattern())
+                        .willReturn(aResponse().withStatus(200).withBody(yml))
+        );
+
+        String serverUrl = buildApiUrl();
+        SourceReference sourceReference = new SourceReference(serverUrl);
+        sourceReference.setHeaderTokenName("PRIVATE_KEY");
+        sourceReference.setHeaderTokenValue("xyz");
+        FileFetcher fetcher = new FileFetcher(new HttpService());
+        String s = fetcher.get(sourceReference);
+        assertEquals(yml, s);
+    }
+
+    @Test
+    void fetchesYamlWithBasicAuth() {
+
+        String path = getRootPath() + "/src/test/resources/example/services/wordpress.yml";
+        String yml = FileFetcher.readFile(new File(path));
+
+        givenThat(
+                get("/some/file.yml")
+                        .withHeader("Authorization", new AnythingPattern())
+                        .willReturn(aResponse().withStatus(200).withBody(yml))
+        );
+
+        String serverUrl = buildApiUrl();
+        SourceReference sourceReference = new SourceReference(serverUrl);
+        sourceReference.setBasicAuthUsername("x");
+        sourceReference.setBasicAuthPassword("y");
         FileFetcher fetcher = new FileFetcher(new HttpService());
         String s = fetcher.get(sourceReference);
         assertEquals(yml, s);
