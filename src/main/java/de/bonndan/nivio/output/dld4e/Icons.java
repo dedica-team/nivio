bdedc27694d476625e5fdc7641fa872a71535bb6
Daniel Pozzi <dpozzi@haeger-consulting.de>
2018-11-15 22:18:50 +0100

    fixed duplicate icons

--- src/main/java/de/bonndan/nivio/output/dld4e/Icons.java
+++ src/main/java/de/bonndan/nivio/output/dld4e/Icons.java
@@ -2,36 +2,35 @@ package de.bonndan.nivio.output.dld4e;
 
 import de.bonndan.nivio.landscape.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.springframework.util.StringUtils.isEmpty;
 
 public class Icons {
 
-    private List<Icon> icons = new ArrayList<>();
+    private Map<Service, Icon> icons = new HashMap<>();
 
     public void add(Service service) {
         Icon icon = new Icon(service);
         icon.merge("service");
         icon.set("icon", "\"" + IconFamily.AzureEnterprise.iconFor(service) + "\"");
-        icons.add(icon);
+        icons.put(service, icon);
     }
 
     public List<Icon> getAll() {
-        return icons;
+        return icons.entrySet().stream()
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
     }
 
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("\nicons:\n");
-        icons.forEach((icon) -> {
-            sb.append("  ")
-                    .append(icon.getService().getIdentifier())
-                    .append(": ")
-                    .append(icon.inline());
-        });
+        icons.forEach((service, icon) -> sb.append("  ")
+                .append(icon.getService().getIdentifier())
+                .append(": ")
+                .append(icon.inline()));
         sb.append("\n");
         return sb.toString();
     }
@@ -41,6 +40,9 @@ public class Icons {
     }
 
     public Optional<Icon> by(Service service) {
-        return icons.stream().filter(icon -> icon.getService() == service).findFirst();
+
+        return icons.entrySet().stream()
+                .map(serviceIconEntry -> serviceIconEntry.getValue())
+                .filter(icon -> icon.getService() == service).findFirst();
     }
 }
