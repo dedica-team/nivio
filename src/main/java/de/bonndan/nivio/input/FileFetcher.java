22c8fbc059f8d9c5dc0e12eed43a2bf6806083fe
Daniel Pozzi <dpozzi@haeger-consulting.de>
2019-01-07 21:41:02 +0100

    improved logging

--- src/main/java/de/bonndan/nivio/input/FileFetcher.java
+++ src/main/java/de/bonndan/nivio/input/FileFetcher.java
@@ -63,8 +63,8 @@ public class FileFetcher {
             }
             return http.get(new URL(ref.getUrl()));
         } catch (IOException | AuthenticationException | URISyntaxException | RuntimeException e) {
-            logger.error("Failed to fetch file " + ref, e);
-            throw new ReadingException(ref.getEnvironment(), "Failed to fetch file "+ ref, e);
+            logger.error("Failed to fetch file " + ref.getUrl(), e);
+            throw new ReadingException(ref.getEnvironment(), "Failed to fetch file "+ ref.getUrl(), e);
         }
     }
 
