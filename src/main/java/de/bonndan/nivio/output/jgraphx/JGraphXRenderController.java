2d634f906635469e312a9cd1970a85d1e0f7e068
Daniel Pozzi <dpozzi@haeger-consulting.de>
2019-02-02 17:45:25 +0100

    mxgraph rendering wip

---
+++ src/main/java/de/bonndan/nivio/output/jgraphx/JGraphXRenderController.java
@@ -0,0 +1,63 @@
+package de.bonndan.nivio.output.jgraphx;
+
+import de.bonndan.nivio.landscape.Landscape;
+import de.bonndan.nivio.landscape.LandscapeRepository;
+import de.bonndan.nivio.output.graphstream.GraphStreamRenderer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.persistence.EntityNotFoundException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+
+@Controller
+@RequestMapping(path = "/renderj")
+public class JGraphXRenderController {
+
+    private static final Logger logger = LoggerFactory.getLogger(de.bonndan.nivio.output.jgraphx.JGraphXRenderController.class);
+
+    private final LandscapeRepository landscapeRepository;
+
+    @Autowired
+    public JGraphXRenderController(LandscapeRepository landscapeRepository) {
+        this.landscapeRepository = landscapeRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{landscape}/graph.png")
+    public ResponseEntity<byte[]> pngResource(@PathVariable(name = "landscape") final String landscapeIdentifier) throws IOException {
+        Landscape landscape = landscapeRepository.findDistinctByIdentifier(landscapeIdentifier);
+        if (landscape == null)
+            throw new EntityNotFoundException("Not found");
+
+        JGraphXRenderer graphStreamRenderer = new JGraphXRenderer();
+        File png = File.createTempFile(landscapeIdentifier, "png");
+        try {
+            graphStreamRenderer.render(landscape, png);
+        } catch (Exception ex) {
+            logger.warn("Could not render graph: " );
+            throw ex;
+        }
+
+        byte[] bFile = Files.readAllBytes(png.toPath());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "image/png");
+        return new ResponseEntity<>(
+                bFile,
+                headers,
+                HttpStatus.OK
+        );
+
+    }
+
+
+}
---
+++ src/main/java/de/bonndan/nivio/output/jgraphx/JGraphXRenderer.java
@@ -0,0 +1,191 @@
+package de.bonndan.nivio.output.jgraphx;
+
+import com.mxgraph.canvas.mxGraphics2DCanvas;
+import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
+import com.mxgraph.util.mxCellRenderer;
+import com.mxgraph.util.mxConstants;
+import com.mxgraph.view.mxGraph;
+import com.mxgraph.view.mxStylesheet;
+import de.bonndan.nivio.landscape.*;
+import de.bonndan.nivio.output.Renderer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class JGraphXRenderer implements Renderer {
+    private final int DEFAULT_ICON_SIZE = 40;
+
+    private static final String[] KNOWN_ICONS = new String[]{
+            "api",
+            "cache",
+            "database",
+            "dataflow",
+            "firewall",
+            "humanuser",
+            "interface",
+            "keyvaluestore",
+            "loadbalancer",
+            "lock",
+            "messagequeue",
+            "mobileclient",
+            "server",
+            "service",
+            "webservice",
+    };
+
+    private Logger logger = LoggerFactory.getLogger(JGraphXRenderer.class);
+    private Map<Service, Object> serviceVertexes = new HashMap<>();
+    private mxStylesheet stylesheet;
+
+    @Override
+    public String render(Landscape landscape) {
+        return null;
+    }
+
+    @Override
+    public void render(Landscape landscape, File file) throws IOException {
+        mxGraph graph = new mxGraph();
+        graph.setHtmlLabels(true);
+
+        stylesheet = graph.getStylesheet();
+        mxGraphics2DCanvas.putShape("circularImage", new mxCircularImageShape());
+
+        graph.getModel().beginUpdate();
+        try {
+            render(graph, landscape);
+        } finally {
+
+            mxHierarchicalLayout layout = new mxHierarchicalLayout(graph);
+            layout.setOrientation(SwingConstants.SOUTH);
+            layout.setInterRankCellSpacing(4 * DEFAULT_ICON_SIZE);
+            layout.setIntraCellSpacing(3 * DEFAULT_ICON_SIZE);
+            layout.execute(graph.getDefaultParent());
+
+            graph.getModel().endUpdate();
+        }
+
+        BufferedImage image = mxCellRenderer.createBufferedImage(graph, null, 1, Color.WHITE, true, null);
+
+        ImageIO.write(image, "PNG", file);
+
+    }
+
+    private void render(mxGraph graph, Landscape landscape) {
+        Object parent = graph.getDefaultParent();
+
+        //add all nodes
+        landscape.getServices().forEach(service -> {
+
+            // "ROUNDED;strokeColor=red;fillColor=green"
+            String style = getBaseStyle(service) + ";" + mxConstants.STYLE_FILLCOLOR + "=#" + de.bonndan.nivio.util.Color.nameToRGB(service.getGroup()) + ";";
+            String statusColor = getStatusColor(service);
+            if (!Status.UNKNOWN.toString().equals(statusColor)) {
+                style += "strokeColor=" + statusColor + ";strokeWidth=3; ";
+            }
+
+            Object v1 = graph.insertVertex(
+                    parent,
+                    service.getFullyQualifiedIdentifier().toString(),
+                    StringUtils.isEmpty(service.getName()) ? service.getIdentifier() : service.getName(),
+                    20, 20, DEFAULT_ICON_SIZE, DEFAULT_ICON_SIZE,
+                    style
+            );
+            serviceVertexes.put(service, v1);
+
+            //label
+//            mxGeometry geo = new mxGeometry(0, 0, 100, 65);
+//            geo.setRelative(true);
+//
+//            mxCell titleCell = new mxCell("Title", geo,"");
+//            titleCell.setVertex(true);
+
+            //graph.addCell(titleCell, v1);
+        });
+
+        //provider edges
+        landscape.getServices().forEach(service -> {
+            service.getProvidedBy().forEach(provider -> {
+
+                String style = mxConstants.STYLE_STROKEWIDTH + "=3;"
+                        + mxConstants.STYLE_ENDARROW + "=false;"
+                        + mxConstants.STYLE_STARTARROW + "=oval;"
+                        + mxConstants.STYLE_STROKECOLOR + "=#" + de.bonndan.nivio.util.Color.nameToRGB(service.getGroup()) + ";";
+                graph.insertEdge(
+                        parent, null, "",
+                        serviceVertexes.get(provider),
+                        serviceVertexes.get(service),
+                        style
+                );
+            });
+        });
+
+        //dataflow
+        landscape.getServices().forEach(service -> service.getDataFlow().forEach(df -> {
+
+            if (df.getSource().equals(df.getTarget()))
+                return;
+
+            String id = "df_" + service.getIdentifier() + df.getTarget();
+            logger.info("Adding dataflow " + id);
+            ServiceItem target = ServiceItems.find(FullyQualifiedIdentifier.from(df.getTarget()), landscape.getServices());
+            graph.insertEdge(parent, id, df.getFormat(), serviceVertexes.get(service), serviceVertexes.get((Service) target),
+                    mxConstants.STYLE_STROKECOLOR + "=#" + de.bonndan.nivio.util.Color.nameToRGB(service.getGroup() + ";" + mxConstants.STYLE_DASHED + "=true")
+            );
+        }));
+
+        //draw vertexes above edges
+        graph.orderCells(false, serviceVertexes.values().toArray());
+    }
+
+    private String getStatusColor(Service service) {
+        var ref = new Object() {
+            Status current = Status.UNKNOWN;
+        };
+
+        service.getStatuses().forEach(statusItem -> {
+            if (statusItem.getStatus().isHigherThan(ref.current))
+                ref.current = statusItem.getStatus();
+        });
+
+        return ref.current.toString();
+    }
+
+    private String getIcon(Service service) {
+        if (StringUtils.isEmpty(service.getType()))
+            return "service";
+
+        //fallback to service
+        if (!Arrays.asList(KNOWN_ICONS).contains(service.getType().toLowerCase()))
+            return "service";
+
+        return service.getType().toLowerCase();
+    }
+
+    private String getBaseStyle(Service service) {
+        String type = getIcon(service);
+
+        if (!stylesheet.getStyles().containsKey(type)) {
+            Hashtable<String, Object> style = new Hashtable<String, Object>();
+            style.put(mxConstants.STYLE_SHAPE, "circularImage");
+            style.put(mxConstants.STYLE_IMAGE, "http://localhost:8080/icons/" + type + ".png");
+            style.put(mxConstants.STYLE_VERTICAL_LABEL_POSITION, mxConstants.ALIGN_BOTTOM);
+            style.put(mxConstants.STYLE_LABEL_BACKGROUNDCOLOR, "#666");
+            style.put(mxConstants.STYLE_FONTCOLOR, "white");
+
+            stylesheet.putCellStyle(type, style);
+        }
+
+        return type;
+    }
+}
