456f8e430468bb14ddc9ce990bdd323b3b7087a4
Daniel Pozzi <bonndan76@googlemail.com>
2020-01-21 10:44:39 +0100

    [#85] cleanup

--- src/main/app/src/App.js
+++ src/main/app/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
             value: INITIAL_VALUE
         }
 
-        this.host = 'http://localhost:8081';
+        this.host = window.location.host;
         let params = new URLSearchParams(window.location.search);
         let host = params.get('host');
         if (host !== null) {
@@ -36,18 +36,6 @@ class App extends Component {
 
     changeValue(nextValue) {
         this.setState({value: nextValue})
-    }
-
-    fitToViewer() {
-        this.Viewer.fitToViewer()
-    }
-
-    fitSelection() {
-        this.Viewer.fitSelection(40, 40, 200, 200)
-    }
-
-    zoomOnViewerCenter() {
-        this.Viewer.zoomOnViewerCenter(1.1)
     }
 
     getLandscapes() {
@@ -144,25 +132,18 @@ class App extends Component {
 
              */
             return <ReactSvgPanZoomLoader src={data} render={(content) => (
-                <div>
-                    <div style={{float: 'right'}}>
-                        <button className="btn" onClick={() => this.zoomOnViewerCenter()}>Zoom in</button>
-                        <button className="btn" onClick={() => this.fitSelection()}>Zoom area 200x200</button>
-                        <button className="btn" onClick={() => this.fitToViewer()}>Fit</button>
-                    </div>
-                    <ReactSVGPanZoom key={'panzoom'}
-                                     width={window.innerWidth * 0.95} height={window.innerHeight * 0.95}
-                                     background={'transparent'}
-                                     miniatureProps={{position: 'none'}} toolbarProps={{position: 'none'}}
-                                     detectAutoPan={false}
-                                     ref={Viewer => this.Viewer = Viewer}
-                                     tool={this.state.tool} onChangeTool={tool => this.changeTool(tool)}
-                                     value={this.state.value} onChangeValue={value => this.changeValue(value)}>
-                        <svg>
-                            {content}
-                        </svg>
-                    </ReactSVGPanZoom>
-                </div>
+                <ReactSVGPanZoom key={'panzoom'}
+                                 width={window.innerWidth * 0.95} height={window.innerHeight * 0.95}
+                                 background={'transparent'}
+                                 miniatureProps={{position: 'none'}} toolbarProps={{position: 'none'}}
+                                 detectAutoPan={false}
+                                 ref={Viewer => this.Viewer = Viewer}
+                                 tool={this.state.tool} onChangeTool={tool => this.changeTool(tool)}
+                                 value={this.state.value} onChangeValue={value => this.changeValue(value)}>
+                    <svg>
+                        {content}
+                    </svg>
+                </ReactSVGPanZoom>
             )}/>
 
         }
