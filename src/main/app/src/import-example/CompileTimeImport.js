97a430ea3d70db1d96ce29be9dc8b45109064e37
Matthias Molitor <matthias@molitor.solutions>
2020-02-03 10:56:28 +0100

    added description

--- src/main/app/src/import-example/CompileTimeImport.js
+++ src/main/app/src/import-example/CompileTimeImport.js
@@ -3,6 +3,13 @@ import raw from "raw.macro";
 
 const text = raw("./example.txt");
 
+/**
+ * This component show how to import files via raw loader.
+ *
+ * Can be removed later.
+ *
+ * @deprecated Only for demonstration.
+ */
 class CompileTimeImport extends Component {
 
     render() {
