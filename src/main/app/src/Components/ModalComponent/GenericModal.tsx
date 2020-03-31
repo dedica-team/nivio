8dad1e388de11823a2a973470ac28167a2381590
Marvin Schöning <schoening.marvin@web.de>
2020-03-31 07:42:46 +0200

    fix shouldCloseOnEsc / shouldCloseOnOverlay in generic modal

--- src/main/app/src/Components/ModalComponent/GenericModal.tsx
+++ src/main/app/src/Components/ModalComponent/GenericModal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, ReactElement } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import Modal from "react-modal";
 
 interface IGenericModalProps{
@@ -9,12 +9,42 @@ const GenericModal: React.FC<IGenericModalProps> = ({modalContent}) => {
 
     Modal.setAppElement("#root");
 
+    const [showModal, setShowModal] = useState(false);
 
-    return <Modal isOpen={modalContent !== null}
+    // We have to save modalContent in a state independent of our props to use shouldCloseOn functions from react-modal
+    // If we just check our prop modalContent it wont be null until WE tell it to outside of our GenericModal
+    // -> showModal would always be true
+    const [modalContentState, setModalContentState] = useState(modalContent);
+
+    useEffect(() => {
+        if (modalContent){
+            setModalContentState(modalContent);
+        } else {
+            setModalContentState(null);
+        }
+    }, [modalContent]);
+
+    useEffect(() => {
+        if (modalContentState){
+            setShowModal(true);
+        }else{
+            setShowModal(false);
+        }
+    }, [modalContentState]);
+
+    // Will only be used if we close it with ESC or clicking on overlay
+    // If we want to close it with a button, we need to set modalContent in our parent component to null
+    const closeModal = () => {
+        setModalContentState(null);
+    }
+
+    return <Modal isOpen={showModal}
                   className="Modal"
                   overlayClassName="Overlay"
                   shouldCloseOnEsc={true}
-                  contentLabel="Modal">{modalContent}</Modal>
+                  shouldCloseOnOverlayClick={true}
+                  onRequestClose={closeModal}
+                  contentLabel="Modal">{modalContentState}</Modal>
 }
 
 export default GenericModal;
