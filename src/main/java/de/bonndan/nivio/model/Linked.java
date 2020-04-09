6eb8af34f68d556b1212160e4fd7bd01541a95be
Daniel Pozzi <bonndan76@googlemail.com>
2020-04-09 10:49:22 +0200

    [#97] preparations for later development

--- src/main/java/de/bonndan/nivio/model/Linked.java
+++ src/main/java/de/bonndan/nivio/model/Linked.java
@@ -1,6 +1,9 @@
 package de.bonndan.nivio.model;
 
+import org.springframework.util.StringUtils;
+
 import java.net.URL;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,9 +13,29 @@ import java.util.Map;
  */
 public interface Linked {
 
+    //TODO add semantics, e.g. handle identifier "sonarqube" to grab metrics
+    // see https://github.com/dedica-team/nivio/issues/97
+    public static final List<String> KNOWN_IDENTIFIERS = List.of(
+            "homepage",
+            "repo",
+            "wiki"
+    );
+
+    String LINK_LABEL_PREFIX = "link.";
+
     /**
      *
      * @return map of links
      */
     Map<String, URL> getLinks();
+
+    default void setLink(String identifier, URL url) {
+        if (StringUtils.isEmpty(identifier)){
+            throw new IllegalArgumentException("Link identifier is empty");
+        }
+        if (KNOWN_IDENTIFIERS.contains(identifier.toLowerCase())) {
+            identifier = identifier.toLowerCase();
+        }
+        getLinks().put(identifier, url);
+    }
 }
