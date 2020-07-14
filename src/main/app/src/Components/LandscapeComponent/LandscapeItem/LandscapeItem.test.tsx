6a1b6fdc810622aa678f5ae85dbfbffb82254d4d
Marvin <schoening.marvin@web.de>
2020-07-14 17:07:04 +0200

    remove unused var

--- src/main/app/src/Components/LandscapeComponent/LandscapeItem/LandscapeItem.test.tsx
+++ src/main/app/src/Components/LandscapeComponent/LandscapeItem/LandscapeItem.test.tsx
@@ -3,7 +3,6 @@ import { render } from '@testing-library/react';
 import LandscapeItem from './LandscapeItem';
 
 it('should render log component', () => {
-  const element = document.createElement('div');
   const { getByText } = render(
     <LandscapeItem fullyQualifiedItemIdentifier={'nivio:example/test'} />
   );
