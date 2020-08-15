d29f1f6475ef89e8df63061edcd45b38139ae852
Daniel Pozzi <daniel@dedica.team>
2020-08-15 06:28:42 +0200

    [#226] more indexed document fields

--- src/main/java/de/bonndan/nivio/model/SearchDocumentFactory.java
+++ src/main/java/de/bonndan/nivio/model/SearchDocumentFactory.java
@@ -1,15 +1,20 @@
 package de.bonndan.nivio.model;
 
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexableFieldType;
+import org.springframework.util.StringUtils;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Optional;
 
 /**
  * Static factory to turn {@link Item}s into Lucene {@link Document}s.
- *
- *
  */
 public class SearchDocumentFactory {
 
@@ -17,13 +22,42 @@ public class SearchDocumentFactory {
     public static final String LUCENE_FIELD_DESCRIPTION = "description";
     public static final String LUCENE_FIELD_CONTACT = "contact";
     public static final String LUCENE_FIELD_FQI = "fqi";
+    public static final String LUCENE_FIELD_COMPONENT_TYPE = "component";
+    public static final String LUCENE_FIELD_GROUP = "group";
+    public static final String LUCENE_FIELD_ITEM_TYPE = "type";
+    public static final String LUCENE_FIELD_OWNER = "owner";
+    public static final String LUCENE_FIELD_TAG = "tag";
 
     public static Document from(Item item) {
         Document document = new Document();
+        document.add(new TextField(LUCENE_FIELD_COMPONENT_TYPE, "item", Field.Store.YES));
         document.add(new TextField(LUCENE_FIELD_FQI, item.getFullyQualifiedIdentifier().toString(), Field.Store.YES));
+
         document.add(new TextField(LUCENE_FIELD_NAME, Optional.ofNullable(item.getName()).orElse(""), Field.Store.YES));
         document.add(new TextField(LUCENE_FIELD_CONTACT, Optional.ofNullable(item.getContact()).orElse(""), Field.Store.YES));
         document.add(new TextField(LUCENE_FIELD_DESCRIPTION, Optional.ofNullable(item.getDescription()).orElse(""), Field.Store.YES));
+        Optional.ofNullable(item.getGroup()).ifPresent(s -> document.add(new TextField(LUCENE_FIELD_GROUP, s, Field.Store.YES)));
+        Optional.ofNullable(item.getType()).ifPresent(s -> document.add(new TextField(LUCENE_FIELD_ITEM_TYPE, s, Field.Store.YES)));
+        Optional.ofNullable(item.getOwner()).ifPresent(s -> document.add(new TextField(LUCENE_FIELD_OWNER, s, Field.Store.YES)));
+
+        //add all labels by their key
+        item.getLabels().forEach((s, s2) -> {
+            if (StringUtils.isEmpty(s2))
+                return;
+            document.add(new TextField(s, s2, Field.Store.YES));
+        });
+
+        //add links, title as key (duplicates are ok)
+        item.getLinks().forEach((s, link) -> {
+            if (link == null)
+                return;
+            String val = StringUtils.isEmpty(link.getName()) ? "" : link.getName() + " ";
+            val += link.getHref();
+            document.add(new TextField(s, val, Field.Store.YES));
+        });
+
+        //tags TODO not clear how
+        document.add(new TextField(LUCENE_FIELD_TAG, StringUtils.arrayToDelimitedString(item.getTags(), " "), Field.Store.YES));
 
         return document;
     }
--- src/test/java/de/bonndan/nivio/model/SearchDocumentFactoryTest.java
+++ src/test/java/de/bonndan/nivio/model/SearchDocumentFactoryTest.java
@@ -3,19 +3,26 @@ package de.bonndan.nivio.model;
 import org.apache.lucene.document.Document;
 import org.junit.jupiter.api.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import static de.bonndan.nivio.model.SearchDocumentFactory.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SearchDocumentFactoryTest {
 
     @Test
-    public void generatesDocument() {
+    public void generatesDocument() throws MalformedURLException {
         //given
         Item item = new Item();
         item.setIdentifier("foo");
         item.setName("Hans");
         item.setDescription("Lorem ipsum");
         item.setContact("info@acme.com");
+        item.setLabel("foo", "bar");
+        item.setLabel("foo2", "bar2");
+        item.setLink("wiki", new URL("http://foo.bar.baz"));
+        item.setTags(new String[]{"one", "two"});
 
         //when
         Document document = SearchDocumentFactory.from(item);
@@ -25,5 +32,15 @@ class SearchDocumentFactoryTest {
         assertEquals(item.getName(), document.get(LUCENE_FIELD_NAME));
         assertEquals(item.getContact(), document.get(LUCENE_FIELD_CONTACT));
         assertEquals(item.getDescription(), document.get(LUCENE_FIELD_DESCRIPTION));
+
+        assertEquals(item.getLabel("foo"), document.get("foo"));
+        assertEquals(item.getLabel("foo2"), document.get("foo2"));
+
+        assertEquals(item.getLinks().get("wiki").getHref().toString(), document.get("wiki"));
+        String tag = document.get("tag");
+        assertTrue(tag.contains("one"));
+        assertTrue(tag.contains("two"));
+
+
     }
 }
