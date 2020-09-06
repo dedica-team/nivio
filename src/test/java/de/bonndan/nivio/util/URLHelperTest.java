723d20d1d32f85997cd730561e40deac7fe82f06
Paul Kusmin <BilligsterUser@protonmail.com>
2020-09-06 19:45:29 +0200

    add  given/when/then to URLHelperTest to enhance readability

--- src/test/java/de/bonndan/nivio/util/URLHelperTest.java
+++ src/test/java/de/bonndan/nivio/util/URLHelperTest.java
@@ -16,31 +16,43 @@ class URLHelperTest {
 
     @Test
     void test_getURL() {
+        //given
         String root = Paths.get("").toAbsolutePath().toString();
+
+        //when
         URL url = URLHelper.getURL(root + "/src/test/resources/example/example_templates.yml");
         if (SystemUtils.IS_OS_WINDOWS) {
             root = "/" + root.replace(File.separator, "/");
         }
+        //then
         assert url != null;
         assertEquals("file:" + root + "/src/test/resources/example/example_templates.yml", url.toString());
     }
 
     @Test
     void test_getURL_relativePath() {
+        //given
         String root = Paths.get("").toAbsolutePath().toString();
         if (SystemUtils.IS_OS_WINDOWS) {
             root = "/" + root.replace(File.separator, "/");
         }
+        //when
         URL url = URLHelper.getURL("src/test/resources/example/example_templates.yml");
+        //then
         assert url != null;
         assertEquals("file:" + root + "/src/test/resources/example/example_templates.yml", url.toString());
     }
 
     @Test
     void test_splitQuery() throws MalformedURLException {
+        //given
         URL url = new URL("http://192.168.99.100:8080?namespace=default&groupLabel=release");
+
+
+        //when
         Map<String, String> actual = URLHelper.splitQuery(url);
 
+        //then
         assertEquals(2, actual.size());
 
         assertTrue(actual.containsKey("namespace"));
@@ -52,21 +64,28 @@ class URLHelperTest {
 
     @Test
     void test_splitQuery_withoutQuery() throws MalformedURLException {
+        //given
         URL url = new URL("http://192.168.99.100:8080");
+        //when
         Map<String, String> actual = URLHelper.splitQuery(url);
+        //then
         assertEquals(0, actual.size());
     }
 
     @Test
     void test_splitQuery_badQuery() throws MalformedURLException {
+        //given
         URL url = new URL("http://192.168.99.100:8080?foo=");
+        //when
         Map<String, String> actual = URLHelper.splitQuery(url);
+        //then
         assertEquals(0, actual.size());
     }
 
 
     @Test
     void test_combine() throws MalformedURLException {
+        //given
         String root = Paths.get("").toAbsolutePath().toString();
         if (SystemUtils.IS_OS_WINDOWS) {
             root = "/" + root.replace(File.separator, "/");
@@ -74,8 +93,10 @@ class URLHelperTest {
         URL baseUrl = new URL("file:" + root + "/src/test/resources/example/");
         String part = "./services/wordpress.yml";
 
+        //when
         String actual = URLHelper.combine(baseUrl, part);
 
+        //then
         String expected = "file:" + root + "/src/test/resources/example/services/wordpress.yml";
 
         assertEquals(expected, actual);
@@ -83,6 +104,7 @@ class URLHelperTest {
 
     @Test
     void test_combine_absolute_part() throws MalformedURLException {
+        //given
         String root = Paths.get("").toAbsolutePath().toString();
         if (SystemUtils.IS_OS_WINDOWS) {
             root = "/" + root.replace(File.separator, "/");
@@ -90,8 +112,10 @@ class URLHelperTest {
         URL baseUrl = new URL("file:" + root + "/src/test/resources/example/");
         String part = "http://192.168.99.100:8080";
 
+        //when
         String actual = URLHelper.combine(baseUrl, part);
 
+        //then
         String expected = "http://192.168.99.100:8080";
 
         assertEquals(expected, actual);
@@ -99,6 +123,7 @@ class URLHelperTest {
 
     @Test
     void test_combine_absolute_part2() throws MalformedURLException {
+        //given
         String root = Paths.get("").toAbsolutePath().toString();
         if (SystemUtils.IS_OS_WINDOWS) {
             root = "/" + root.replace(File.separator, "/");
@@ -106,8 +131,10 @@ class URLHelperTest {
         URL baseUrl = new URL("file:" + root + "/src/test/resources/example/");
         String part = "file:" + root + "/src/test/resources/example/example_templates.yml";
 
+        //when
         String actual = URLHelper.combine(baseUrl, part);
 
+        //then
         assertEquals(part, actual);
     }
 }
