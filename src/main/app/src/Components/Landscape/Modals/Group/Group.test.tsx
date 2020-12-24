6e780c0f626bf9c00ec8a9452ae98d86fbc8ea14
Daniel Pozzi <daniel@dedica.team>
2020-12-24 15:32:37 +0100

    test fix

--- src/main/app/src/Components/Landscape/Modals/Group/Group.test.tsx
+++ src/main/app/src/Components/Landscape/Modals/Group/Group.test.tsx
@@ -3,7 +3,13 @@ import { render } from '@testing-library/react';
 import Group from './Group';
 
 it('should render landscape group component', () => {
-  const group = {identifier: 'foo', fullyQualifiedIdentifier: 'bar/foo', items: []};
-  const { getByText } = render(<Group group={group} assessments={null}/>);
-  expect(getByText('Error Loading Group')).toBeInTheDocument();
+  const group = {identifier: 'foo', name: 'foo', fullyQualifiedIdentifier: 'bar/foo', items: []};
+  const assessments = {
+    results: {
+
+    },
+    date: '',
+  }
+  const { getByText } = render(<Group group={group} assessments={assessments}/>);
+  expect(getByText('Group foo')).toBeInTheDocument();
 });
