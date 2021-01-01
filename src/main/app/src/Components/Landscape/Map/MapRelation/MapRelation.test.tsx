621baec47ad83f44478babba342ef3cd62d1fa89
Daniel Pozzi <daniel@dedica.team>
2021-01-01 20:23:10 +0100

    [#349] test fix

--- src/main/app/src/Components/Landscape/Map/MapRelation/MapRelation.test.tsx
+++ src/main/app/src/Components/Landscape/Map/MapRelation/MapRelation.test.tsx
@@ -3,15 +3,43 @@ import { render } from '@testing-library/react';
 import MapRelation from './MapRelation';
 
 it('should render mapRelation component', () => {
+  const source = {
+    identifier: 'foo',
+    name: 'fooName',
+    owner: '',
+    contact: '',
+    relations: {},
+    labels: {},
+    tags: [],
+    icon: '',
+    type: 'service',
+    fullyQualifiedIdentifier: 'abc/foo',
+  };
+  const target = {
+    identifier: 'bar',
+    name: 'barName',
+    owner: '',
+    contact: '',
+    relations: {},
+    labels: {},
+    tags: [],
+    icon: '',
+    type: 'service',
+    fullyQualifiedIdentifier: 'abc/bar',
+  };
+
+  const relation =  {
+    source: source.fullyQualifiedIdentifier,
+    target: target.fullyQualifiedIdentifier,
+    type: 'PROVIDER',
+    id: target.fullyQualifiedIdentifier,
+    direction: 'outbound',
+    name: 'bar'
+  };
   const { getByText } = render(
-    <MapRelation
-      sourceIdentifier={'source/test/source'}
-      targetIdentifier={'target/test/target'}
-      type='typeTest'
-      locateItem={() => {}}
-    />
+    <MapRelation source={source} target={target} relation={relation} locateItem={() => {}} />
   );
-  expect(getByText('test/source')).toBeInTheDocument();
-  expect(getByText('test/target')).toBeInTheDocument();
-  expect(getByText('Type: typeTest')).toBeInTheDocument();
+  expect(getByText('fooName')).toBeInTheDocument();
+  expect(getByText('barName')).toBeInTheDocument();
+  expect(getByText('Type: PROVIDER')).toBeInTheDocument();
 });
