9828386cab8f451badc34b388bd0e7507992a487
Daniel Pozzi <daniel@dedica.team>
2021-01-10 13:15:31 +0100

    [#246] hiding debug messages

--- src/main/app/src/Components/Landscape/Modals/Log/Log.tsx
+++ src/main/app/src/Components/Landscape/Modals/Log/Log.tsx
@@ -1,63 +1,63 @@
-import React, {useState, useEffect, useCallback} from 'react';
-import {ILandscape} from '../../../../interfaces';
-import {get} from '../../../../utils/API/APIClient';
+import React, { useState, useEffect, useCallback } from 'react';
+import { ILandscape } from '../../../../interfaces';
+import { get } from '../../../../utils/API/APIClient';
 
 import LevelChip from '../../../LevelChip/LevelChip';
-import {Card, CardHeader} from '@material-ui/core';
+import { Card, CardHeader } from '@material-ui/core';
 import componentStyles from '../../../../Ressources/styling/ComponentStyles';
-import CardContent from "@material-ui/core/CardContent";
+import CardContent from '@material-ui/core/CardContent';
 
 interface Props {
-    landscape: ILandscape;
+  landscape: ILandscape;
 }
 
 interface Entry {
-    level: string;
-    message: string;
-    date: string;
+  level: string;
+  message: string;
+  date: string;
 }
 
 /**
  * Gets all logs from the backend for a landscape
  * @param landscape Landscape of which the log is to be shown
  */
-const Log: React.FC<Props> = ({landscape}) => {
-    const [data, setData] = useState<Entry[] | null>(null);
-    const [loadData, setLoadData] = useState<boolean>(true);
-    const classes = componentStyles();
+const Log: React.FC<Props> = ({ landscape }) => {
+  const [data, setData] = useState<Entry[] | null>(null);
+  const [loadData, setLoadData] = useState<boolean>(true);
+  const classes = componentStyles();
 
-    const getLog = useCallback(async () => {
-        if (loadData) {
-            const log: any = await get(`/api/landscape/${landscape.identifier}/log`);
-            if (log) {
-                setData(log.messages);
-                setLoadData(false);
-            }
-        }
-    }, [loadData, landscape]);
+  const getLog = useCallback(async () => {
+    if (loadData) {
+      const log: any = await get(`/api/landscape/${landscape.identifier}/log`);
+      if (log) {
+        setData(log.messages);
+        setLoadData(false);
+      }
+    }
+  }, [loadData, landscape]);
 
-    useEffect(() => {
-        getLog();
-    }, [getLog]);
+  useEffect(() => {
+    getLog();
+  }, [getLog]);
 
-    const content = data?.map((m, i) => {
-        return (
-            <div key={i}>
-                <LevelChip level={m.level} title={m.date}/><br/>
-                <span className='logMessage'>{m.message}</span>
-            </div>
-        );
-    });
-
-    let title = 'Process Log of ' + landscape.name;
+  const content = data?.map((m, i) => {
+    if (m.level === 'DEBUG') return;
     return (
-        <Card className={classes.card}>
-            <CardHeader title={title}/>
-            <CardContent>
-                {content}
-            </CardContent>
-        </Card>
+      <div key={i}>
+        <LevelChip level={m.level} title={m.date} />
+        <br />
+        <span className='logMessage'>{m.message}</span>
+      </div>
     );
+  });
+
+  let title = 'Process Log of ' + landscape.name;
+  return (
+    <Card className={classes.card}>
+      <CardHeader title={title} />
+      <CardContent>{content}</CardContent>
+    </Card>
+  );
 };
 
 export default Log;
