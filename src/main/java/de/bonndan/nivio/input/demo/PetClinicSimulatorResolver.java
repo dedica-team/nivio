8a94c890d17c79650fdba7311626a5f2afdb98c5
Daniel Pozzi <daniel@dedica.team>
2021-06-04 15:26:43 +0200

    pet clinic simulator test (radiation scales down customerDB)

--- src/main/java/de/bonndan/nivio/input/demo/PetClinicSimulatorResolver.java
+++ src/main/java/de/bonndan/nivio/input/demo/PetClinicSimulatorResolver.java
@@ -1,5 +1,6 @@
 package de.bonndan.nivio.input.demo;
 
+import de.bonndan.nivio.assessment.kpi.HealthKPI;
 import de.bonndan.nivio.input.ProcessLog;
 import de.bonndan.nivio.input.Resolver;
 import de.bonndan.nivio.input.dto.ItemDescription;
@@ -13,48 +14,57 @@ import java.util.*;
  */
 public class PetClinicSimulatorResolver extends Resolver {
 
+    public static final String LANDSCAPE_IDENTIFIER_PETCLINIC = "petclinic";
+    public static final String RADIATION = "radiation";
+
     public PetClinicSimulatorResolver(ProcessLog processLog) {
         super(processLog);
     }
 
     @Override
     public void resolve(LandscapeDescription input) {
-        if (!input.getIdentifier().equals("petclinic")) {
+        if (!input.getIdentifier().equals(LANDSCAPE_IDENTIFIER_PETCLINIC)) {
             return;
         }
 
-        simulateScale(input);
+        simulateHealth(input);
         simulateRadiation(input);
     }
 
-    private void simulateScale(LandscapeDescription input) {
+    private void simulateHealth(LandscapeDescription input) {
         List<ItemDescription> all = new ArrayList<>(input.getItemDescriptions().all());
-        Collections.shuffle(all);
-        Optional.ofNullable(all.get(0)).ifPresent(itemDescription -> {
-            int scale = 1;
-            try {
-                scale = Integer.parseInt(itemDescription.getLabel(Label.scale));
-            } catch (NumberFormatException ignored) {
-                //the item had no or an invalid scale label, which is ok for the simulation
-            }
-            if (scale <= 0) {
-                itemDescription.setLabel(Label.scale, "1");
-            } else {
-                itemDescription.setLabel(Label.scale, "0");
-            }
-            processLog.info(String.format("Simulating scale %s on item %s", itemDescription.getLabel(Label.scale), itemDescription));
-        });
+
+        ItemDescription picked = all.stream()
+                .filter(itemDescription -> Optional.ofNullable(itemDescription.getLabel(Label.health)).map(s -> s.equals(HealthKPI.UNHEALTHY)).orElse(false))
+                .findFirst()
+                .orElseGet(() -> {
+                    Collections.shuffle(all);
+                    return Optional.ofNullable(all.get(0)).orElse(null);
+                });
+
+        String health = Optional.ofNullable(picked.getLabel(Label.health)).orElse(HealthKPI.HEALTHY);
+        picked.setLabel(Label.health, health.equals(HealthKPI.UNHEALTHY) ? HealthKPI.HEALTHY : HealthKPI.UNHEALTHY);
+        processLog.info(String.format("Simulating health %s on item %s", picked.getLabel(Label.health), picked));
     }
 
     private void simulateRadiation(LandscapeDescription input) {
         Random r = new Random();
-        int low = 0;
+        int low = 1;
         int high = 1000;
         int mrem = r.nextInt(high - low) + low;
 
         ItemDescription sensor = input.getItemDescriptions().pick("sensor", "xray");
+        int currentRad = Integer.parseInt(Optional.ofNullable(sensor.getLabel(RADIATION)).orElse("0"));
+        if (mrem == currentRad) {
+            mrem--;
+        }
         processLog.info(String.format("Simulating radiation of %d mrem on item %s", mrem, sensor));
-        sensor.setLabel("radiation", mrem);
+        sensor.setLabel(RADIATION, mrem);
+
+        ItemDescription customerDb = input.getItemDescriptions().pick("customer-db", "xray");
+        int scale = mrem > 300 ? 0 : 1;
+        customerDb.setLabel(Label.scale, String.valueOf(scale));
+        processLog.info(String.format("Customer DB is scaled to %s", scale));
     }
 
 }
