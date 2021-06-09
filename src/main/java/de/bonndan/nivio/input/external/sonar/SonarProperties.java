b84c22ef0023302ec1977c2361defe36ba772ee3
BriceNtchoumou <Brice.Ntchoumou@rwth-aachen.de>
2021-06-09 17:41:22 +0200

    create a new class of the environment variables SONAR

---
+++ src/main/java/de/bonndan/nivio/input/external/sonar/SonarProperties.java
@@ -0,0 +1,59 @@
+package de.bonndan.nivio.input.external.sonar;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@ConfigurationProperties(prefix = "sonar")
+@Validated
+
+public class SonarProperties {
+
+    public String getServerUrl() {
+        return serverUrl;
+    }
+
+    public void setServerUrl(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProxyHost() {
+        return proxyHost;
+    }
+
+    public void setProxyHost(String proxyHost) {
+        this.proxyHost = proxyHost;
+    }
+
+    public String getProxyPort() {
+        return proxyPort;
+    }
+
+    public void setProxyPort(String proxyPort) {
+        this.proxyPort = proxyPort;
+    }
+
+    private String serverUrl;
+    private String login;
+    private String password;
+    private String proxyHost;
+    private String proxyPort;
+
+
+
+}
