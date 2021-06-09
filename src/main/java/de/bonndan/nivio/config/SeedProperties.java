f5d3041d093f6973875da5170059b344aebaa9a1
BriceNtchoumou <Brice.Ntchoumou@rwth-aachen.de>
2021-06-09 17:30:18 +0200

    create a new class of the environment variable SEED

---
+++ src/main/java/de/bonndan/nivio/config/SeedProperties.java
@@ -0,0 +1,19 @@
+package de.bonndan.nivio.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@ConfigurationProperties(prefix = "nivio")
+@Validated
+public class SeedProperties {
+    public String getSeed() {
+        return seed;
+    }
+
+    public void setSeed(String seed) {
+        this.seed = seed;
+    }
+
+    private String seed;
+
+}
