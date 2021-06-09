6bbf906247f0318933795c194b0b32b8b62b5771
BriceNtchoumou <Brice.Ntchoumou@rwth-aachen.de>
2021-06-09 17:46:38 +0200

    create a new class of the environment variables GITLAB

---
+++ src/main/java/de/bonndan/nivio/input/external/gitlab/GitLabProperties.java
@@ -0,0 +1,47 @@
+package de.bonndan.nivio.input.external.gitlab;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@ConfigurationProperties(prefix = "gitlab")
+@Validated
+public class GitLabProperties {
+
+    public String getHostUrl() {
+        return hostUrl;
+    }
+
+    public void setHostUrl(String hostUrl) {
+        this.hostUrl = hostUrl;
+    }
+
+    public String getPersonalAccessToken() {
+        return personalAccessToken;
+    }
+
+    public void setPersonalAccessToken(String personalAccessToken) {
+        this.personalAccessToken = personalAccessToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String hostUrl;
+    private String personalAccessToken;
+    private String username;
+    private String password;
+
+}
