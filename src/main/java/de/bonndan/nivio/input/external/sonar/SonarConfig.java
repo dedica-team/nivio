6b8f48289b4a6204f11068f7ca00e42381a1d229
BriceNtchoumou <Brice.Ntchoumou@rwth-aachen.de>
2021-06-15 08:05:51 +0200

    update the method getSonarBuilder()

--- src/main/java/de/bonndan/nivio/input/external/sonar/SonarConfig.java
+++ src/main/java/de/bonndan/nivio/input/external/sonar/SonarConfig.java
@@ -1,7 +1,6 @@
 package de.bonndan.nivio.input.external.sonar;
 
 import org.sonar.wsclient.SonarClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Optional;
 
 import static de.bonndan.nivio.config.ConfigurableEnvVars.*;
+import java.util.Optional;
 
 @Configuration
 @EnableConfigurationProperties(SonarProperties.class)
@@ -16,7 +16,6 @@ public class SonarConfig {
 
     private final SonarProperties sonarProperties;
 
-    @Autowired
     public SonarConfig(SonarProperties sonarProperties) {
         this.sonarProperties = sonarProperties;
     }
@@ -24,16 +23,19 @@ public class SonarConfig {
     @Bean
     public SonarClient.Builder getSonarClientBuilder() {
 
-        Optional<String> serverUrl = SONAR_SERVER_URL.value();
+        Optional<String> serverUrl = Optional.ofNullable(sonarProperties.getServerUrl());
+
         if (serverUrl.isEmpty()) {
             return null;
         }
         SonarClient.Builder builder = SonarClient.builder().url(serverUrl.get());
 
-        SONAR_LOGIN.value().ifPresent(builder::login);
-        SONAR_PASSWORD.value().ifPresent(builder::password);
-        SONAR_PROXY_HOST.value().ifPresent(
-                host -> SONAR_PROXY_PORT.value()
+
+       Optional.ofNullable(sonarProperties.getLogin()) .ifPresent(builder::login);
+
+        Optional.ofNullable(sonarProperties.getPassword()).ifPresent(builder::password);
+        Optional.ofNullable(sonarProperties.getProxyHost()).ifPresent(
+                host -> Optional.ofNullable(sonarProperties.getProxyPort())
                         .ifPresent(port -> builder.proxy(host, Integer.parseInt(port)))
         );
 
