1972fc76420ab590ab15ab88a042b94a5474ef2f
BriceNtchoumou <Brice.Ntchoumou@rwth-aachen.de>
2021-07-06 11:31:53 +0200

    remove the unused import and the duplicate import

--- src/main/java/de/bonndan/nivio/input/external/sonar/SonarProperties.java
+++ src/main/java/de/bonndan/nivio/input/external/sonar/SonarProperties.java
@@ -1,7 +1,7 @@
 package de.bonndan.nivio.input.external.sonar;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
+
 
 @ConfigurationProperties(prefix = "sonar")
 
