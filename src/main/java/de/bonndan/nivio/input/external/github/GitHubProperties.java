d28b5f817003687c0b9cb5f3b97fbf622a65ce43
BriceNtchoumou <Brice.Ntchoumou@rwth-aachen.de>
2021-07-06 11:30:21 +0200

    removed the unused import org.springframework.validation.annotation.Validated

--- src/main/java/de/bonndan/nivio/input/external/github/GitHubProperties.java
+++ src/main/java/de/bonndan/nivio/input/external/github/GitHubProperties.java
@@ -1,7 +1,7 @@
 package de.bonndan.nivio.input.external.github;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
+
 
 @ConfigurationProperties(prefix = "github")
 
