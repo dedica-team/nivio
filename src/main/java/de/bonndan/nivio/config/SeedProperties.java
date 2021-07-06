273f3aa4c4c5e692c8993dab5888db2cf05c8ec6
BriceNtchoumou <Brice.Ntchoumou@rwth-aachen.de>
2021-07-06 11:29:10 +0200

    removed the unused import org.springframework.validation.annotation.Validated

--- src/main/java/de/bonndan/nivio/config/SeedProperties.java
+++ src/main/java/de/bonndan/nivio/config/SeedProperties.java
@@ -1,7 +1,7 @@
 package de.bonndan.nivio.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.validation.annotation.Validated;
+
 
 @ConfigurationProperties(prefix = "nivio")
 
