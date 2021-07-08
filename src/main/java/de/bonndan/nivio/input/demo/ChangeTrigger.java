8e68089a001604ea526eb52050fbdffe3152f6bc
BriceNtchoumou <Brice.Ntchoumou@rwth-aachen.de>
2021-07-08 11:02:31 +0200

    add the variable seedProperties and modifies the constructor and the method trigger()

--- src/main/java/de/bonndan/nivio/input/demo/ChangeTrigger.java
+++ src/main/java/de/bonndan/nivio/input/demo/ChangeTrigger.java
@@ -1,6 +1,7 @@
 package de.bonndan.nivio.input.demo;
 
 import de.bonndan.nivio.config.ConfigurableEnvVars;
+import de.bonndan.nivio.config.SeedProperties;
 import de.bonndan.nivio.input.IndexEvent;
 import de.bonndan.nivio.input.LandscapeDescriptionFactory;
 import de.bonndan.nivio.input.dto.LandscapeDescription;
@@ -21,18 +22,21 @@ public class ChangeTrigger {
 
     private final LandscapeDescriptionFactory landscapeDescriptionFactory;
     private final ApplicationEventPublisher eventPublisher;
+    private final SeedProperties seedProperties;
 
     public ChangeTrigger(LandscapeDescriptionFactory landscapeDescriptionFactory,
-                         ApplicationEventPublisher eventPublisher
+                         ApplicationEventPublisher eventPublisher,
+                         SeedProperties seedProperties
     ) {
         this.landscapeDescriptionFactory = landscapeDescriptionFactory;
         this.eventPublisher = eventPublisher;
+        this.seedProperties = seedProperties;
     }
 
     @Scheduled(initialDelay = 20000, fixedDelay = 30000)
     public void trigger() {
 
-        if (ConfigurableEnvVars.DEMO.value().isEmpty()) {
+        if (seedProperties.getSeed().isEmpty()) {
             LOGGER.debug("DEMO not set, not simulating any pet clinic events.");
             return;
         }
