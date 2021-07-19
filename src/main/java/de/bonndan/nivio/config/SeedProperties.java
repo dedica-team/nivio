a201510ec74f9a1a028d62d34c3596c89f5f71b8
BriceNtchoumou <Brice.Ntchoumou@rwth-aachen.de>
2021-07-19 13:53:33 +0200

    add the new attribute demo and the set/get methods

--- src/main/java/de/bonndan/nivio/config/SeedProperties.java
+++ src/main/java/de/bonndan/nivio/config/SeedProperties.java
@@ -2,10 +2,13 @@ package de.bonndan.nivio.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.Optional;
+
 @ConfigurationProperties(prefix = "nivio")
 public class SeedProperties {
 
     private String seed;
+    private String demo; // add this attribute for the environment variable DEMO
 
     public String getSeed() {
         return seed;
@@ -14,4 +17,8 @@ public class SeedProperties {
     public void setSeed(String seed) {
         this.seed = seed;
     }
+
+    public String getDemo(){return demo;}
+
+    public void setDemo(String demo){this.demo = demo;}
 }
