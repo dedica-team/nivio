59b44b86f0f521d4c407186a121b26df16cf6795
Daniel Pozzi <daniel@dedica.team>
2021-07-31 08:20:29 +0200

    bumped databind version, using ZonedDateTime instead of LocalDateTime

--- src/main/java/de/bonndan/nivio/config/ApplicationConfig.java
+++ src/main/java/de/bonndan/nivio/config/ApplicationConfig.java
@@ -5,6 +5,7 @@ import de.bonndan.nivio.output.icons.LocalIcons;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -37,6 +38,11 @@ public class ApplicationConfig {
     @Bean
     public LocalIcons getLocalIcons(@Value("${nivio.iconFolder:" + DEFAULT_ICONS_FOLDER + "}") String iconsFolder) {
         return new LocalIcons(iconsFolder);
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder();
     }
 
 }
