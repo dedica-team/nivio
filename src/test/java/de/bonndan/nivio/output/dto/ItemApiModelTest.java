059b926e77038918d1419a7c1358389a3bcfc685
robert <robert@bitterling.de>
2021-08-10 16:31:22 +0200

    Add tests

--- src/test/java/de/bonndan/nivio/output/dto/ItemApiModelTest.java
+++ src/test/java/de/bonndan/nivio/output/dto/ItemApiModelTest.java
@@ -4,6 +4,11 @@ import de.bonndan.nivio.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.Set;
+
 import static de.bonndan.nivio.model.ItemFactory.getTestItemBuilder;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,9 +20,17 @@ class ItemApiModelTest {
 
     @BeforeEach
     void setUp() {
-         landscape = LandscapeFactory.createForTesting("l1", "l1Landscape").build();
-         group = new Group("g1", landscape.getIdentifier());
-         itemTemplate = getTestItemBuilder("g1", "a").withLandscape(landscape).withType("two");
+        landscape = LandscapeFactory.createForTesting("l1", "l1Landscape").build();
+        group = new Group("g1", landscape.getIdentifier());
+        try {
+            itemTemplate = getTestItemBuilder("g1", "a").withAddress(new URI("https://dedica.team/"))
+                    .withDescription("testDescription")
+                    .withOwner("testOwner")
+                    .withIcon("testIcon")
+                    .withContact("testContact")
+                    .withLandscape(landscape).withType("two");
+        } catch (URISyntaxException ignored) {
+        }
     }
 
     @Test
@@ -61,5 +74,109 @@ class ItemApiModelTest {
 
         //then
         assertThat(itemApiModel.getIcon()).isEqualTo(s1.getLabel(Label._icondata));
+    }
+
+    @Test
+    void getIdentifier() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.getIdentifier()).isEqualTo("a");
+    }
+
+    @Test
+    void getFullyQualifiedIdentifier() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.getFullyQualifiedIdentifier()).isEqualTo(FullyQualifiedIdentifier.build("l1", "g1", "a"));
+    }
+
+    @Test
+    void getName() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.getName()).isEqualTo("");
+    }
+
+    @Test
+    void getOwner() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.getOwner()).isEqualTo("testOwner");
+    }
+
+    @Test
+    void getContact() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.getContact()).isEqualTo("testContact");
+    }
+
+    @Test
+    void getGroup() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.getGroup()).isEqualTo("g1");
+    }
+
+    @Test
+    void getDescription() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.getDescription()).isEqualTo("testDescription");
+    }
+
+    @Test
+    void getJSONRelations() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.getJSONRelations()).isEqualTo(Map.of());
+    }
+
+    @Test
+    void getType() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.getType()).isEqualTo("two");
+    }
+
+    @Test
+    void getAddress() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.getAddress()).isEqualTo("https://dedica.team/");
+    }
+
+    @Test
+    void getInterfaces() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.getInterfaces()).isEqualTo(Set.of());
+    }
+
+    @Test
+    void getTags() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.getTags()).isEqualTo(new String[0]);
+    }
+
+    @Test
+    void testToString() {
+        Item s1 = itemTemplate.build();
+        ItemApiModel itemApiModel = new ItemApiModel(s1, group);
+        assertThat(itemApiModel.getColor()).isEqualTo(group.getColor());
+        assertThat(itemApiModel.toString()).hasToString("l1/g1/a");
     }
 }
