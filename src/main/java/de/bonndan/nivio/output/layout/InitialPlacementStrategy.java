774634cd9f40d012cee27bd89e2a55261e8c2e6e
Daniel Pozzi <daniel@dedica.team>
2021-09-20 17:30:25 +0200

    initial placement of groups regarding dimensions

--- src/main/java/de/bonndan/nivio/output/layout/InitialPlacementStrategy.java
+++ src/main/java/de/bonndan/nivio/output/layout/InitialPlacementStrategy.java
@@ -1,5 +1,7 @@
 package de.bonndan.nivio.output.layout;
 
+import org.springframework.lang.NonNull;
+
 import java.awt.geom.Point2D;
 import java.util.List;
 
@@ -10,19 +12,30 @@ import java.util.List;
  */
 public class InitialPlacementStrategy {
 
-    private final List<LayoutedComponent> bounds;
     private final Point2D.Double origin = new Point2D.Double(0, 0);
-    private final int radius = 50;
+    private final double[] approxRadius;
+    private final double approxRadiusSum;
 
-    public InitialPlacementStrategy(List<LayoutedComponent> bounds) {
-        this.bounds = bounds;
+    public InitialPlacementStrategy(@NonNull final List<LayoutedComponent> bounds) {
+        approxRadius = new double[bounds.size()];
+        var sum = 0;
+        for (int i = 0, boundsSize = bounds.size(); i < boundsSize; i++) {
+            LayoutedComponent layoutedComponent = bounds.get(i);
+            double width = layoutedComponent.getWidth();
+            double height = layoutedComponent.getHeight();
+            approxRadius[i] = Math.max(width, height);
+            sum += approxRadius[i];
+        }
+        approxRadiusSum = sum;
     }
 
     public Point2D.Double place(int i) {
 
-        double t = 2 * Math.PI * i / bounds.size();
-        int x = (int) Math.round(origin.x + radius * Math.cos(t));
-        int y = (int) Math.round(origin.y + radius * Math.sin(t));
+        var share = approxRadius[i] / approxRadiusSum;
+        double slice = 2 * Math.PI * i / share;
+
+        int x = (int) Math.round(origin.x + approxRadius[i] * Math.cos(slice));
+        int y = (int) Math.round(origin.y + approxRadius[i] * Math.sin(slice));
 
         return new Point2D.Double(x, y);
     }
