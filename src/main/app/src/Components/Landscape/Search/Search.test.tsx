b4faeb25dfa94a1479d1f05b7027bf059a710094
robert <robert@bitterling.de>
2021-11-10 09:21:15 +0100

    Add test

--- src/main/app/src/Components/Landscape/Search/Search.test.tsx
+++ src/main/app/src/Components/Landscape/Search/Search.test.tsx
@@ -1,10 +1,12 @@
-import { render } from '@testing-library/react';
+import { fireEvent, getByTitle, render, waitFor } from '@testing-library/react';
 import React from 'react';
 import Search from './Search';
 import { LandscapeContext } from '../../../Context/LandscapeContext';
 import landscapeContextValue from '../../../utils/testing/LandscapeContextValue';
 import { createTheme, ThemeOptions, ThemeProvider } from '@material-ui/core/styles';
 import defaultThemeVariables from '../../../Resources/styling/theme';
+import { IFacet } from '../../../interfaces';
+import * as APIClient from '../../../utils/API/APIClient';
 
 function MockTheme({ children }: any) {
   const tv: ThemeOptions = defaultThemeVariables;
@@ -19,6 +21,40 @@ function MockTheme({ children }: any) {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
 
+const facets: IFacet[] = [
+  {
+    dim: 'kpi_security',
+    value: 19,
+    labelValues: [
+      {
+        label: 'green',
+        value: 12,
+        color: '#161618',
+      },
+      {
+        label: 'red',
+        value: 7,
+        color: '#161618',
+      },
+    ],
+  },
+  {
+    dim: 'owner',
+    value: 12,
+    labelValues: [
+      {
+        label: 'foo',
+        value: 5,
+        color: '',
+      },
+      {
+        label: 'bar',
+        value: 7,
+        color: '#161618',
+      },
+    ],
+  },
+];
 describe('<Search />', () => {
   it('should render', () => {
     const { getByText } = render(
@@ -29,5 +65,25 @@ describe('<Search />', () => {
       </LandscapeContext.Provider>
     );
     expect(getByText('Search')).toBeInTheDocument();
+  });
+  it('should add facet to search term', async () => {
+    const mock = jest.spyOn(APIClient, 'get');
+    mock.mockReturnValue(Promise.resolve(facets));
+    const { container, getByText, getByDisplayValue } = render(
+      <LandscapeContext.Provider value={landscapeContextValue}>
+        <MockTheme>
+          <Search showSearch={() => {}} setSidebarContent={() => {}} />
+        </MockTheme>
+      </LandscapeContext.Provider>
+    );
+
+    fireEvent.click(getByTitle(container, 'Fields'));
+
+    await waitFor(() => getByText('foo'));
+    const fooFacetChip = getByText('foo');
+    expect(fooFacetChip).toBeInTheDocument();
+
+    fireEvent.click(fooFacetChip);
+    expect(getByDisplayValue('owner:foo')).toBeInTheDocument();
   });
 });
