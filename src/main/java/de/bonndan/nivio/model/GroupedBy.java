12bdb7dceed8a6cb911df710f77e8af41108c137
BriceNtchoumou <Brice.Ntchoumou@rwth-aachen.de>
2021-11-16 05:08:19 +0100

    add the methods newBy() to group services by the field labels and the method newFunc() to group service by the field results

--- src/main/java/de/bonndan/nivio/model/GroupedBy.java
+++ src/main/java/de/bonndan/nivio/model/GroupedBy.java
@@ -1,5 +1,6 @@
 package de.bonndan.nivio.model;
 
+import de.bonndan.nivio.assessment.StatusValue;
 import org.springframework.lang.NonNull;
 
 import java.util.*;
@@ -14,9 +15,9 @@ public class GroupedBy {
 
     private final Map<String, List<Item>> groups = new HashMap<>();
 
+
     /**
      * Groups services by any string field (e.g. owner).
-     *
      * @param supplier function providing the group key for each item
      * @param items    items to group
      * @return grouped items
@@ -27,6 +28,35 @@ public class GroupedBy {
         items.forEach(item -> groups.add(supplier.apply(item), item));
         return groups;
     }
+    /**
+     *Groups services by the Map<String,String> field (e.g. labels).
+     * @param sup function providing the group key for each item
+     * @param items    items to group
+     * @return grouped items
+     */
+
+    @NonNull
+    public static GroupedBy newBy(Function<Item,Map<String,String>> sup, Collection<Item> items){
+        var groups = new GroupedBy();
+        String key = "lifecycle";
+        items.forEach(item -> groups.add(sup.apply(item).get(key),item));
+        return groups;
+    }
+
+    /**
+     *Groups services by the Map<String,List<StatusValue>> field (e.g. results)
+     * @param newSupplier function providing the group key for each item
+     * @param items    items to group
+     * @return grouped items
+     */
+    @NonNull
+   public static GroupedBy newFunc(Function<Item,Map<String,List<StatusValue>>> newSupplier,Collection<Item>items){
+        var groups = new GroupedBy();
+        items.forEach(item -> groups.add(newSupplier.apply(item).get(item.getAssessmentIdentifier()).toString(),item));
+        return groups;
+   }
+
+
 
     /**
      * Returns the services sorted by group.
