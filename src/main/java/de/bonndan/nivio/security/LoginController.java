3b6832a592d0c917e09bcbdc8b969825a6b1aa64
Jennifer Arps <jennifer@dedica.team>
2021-12-13 12:31:27 +0100

    [#741] add github login page

--- src/main/java/de/bonndan/nivio/security/LoginController.java
+++ src/main/java/de/bonndan/nivio/security/LoginController.java
@@ -1,25 +1,30 @@
 package de.bonndan.nivio.security;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
-@RestController
+@Controller
 public class LoginController {
 
+//        @CrossOrigin(methods = RequestMethod.GET)
+//        @GetMapping(path = "/user")
+//        public ResponseEntity<String> whoAmI (OAuth2AuthenticationToken principal){
+//        if (principal != null) {
+//            return ResponseEntity.of(Optional.ofNullable(principal.getPrincipal().getAttribute("login")));
+//        } else {
+//            return ResponseEntity.of(Optional.of("anonymous"));
+//        }
+
+
     @CrossOrigin(methods = RequestMethod.GET)
-    @GetMapping(path = "/user")
-    public ResponseEntity<String> whoAmI(OAuth2AuthenticationToken principal) {
-        if (principal != null) {
-            return ResponseEntity.of(Optional.ofNullable(principal.getPrincipal().getAttribute("login")));
-        } else {
-            return ResponseEntity.of(Optional.of("anonymous"));
-        }
+    @GetMapping(path = "/login")
+
+    public String showLoginPage() {
+        return "login";
     }
+
 
 }
--- src/main/java/de/bonndan/nivio/security/SecurityConfig.java
+++ src/main/java/de/bonndan/nivio/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package de.bonndan.nivio.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,13 +13,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .and().oauth2Login().defaultSuccessUrl("/")
-                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID");
+        http.authorizeRequests()
+                .antMatchers("/", "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .loginPage("/login")
+                .and()
+                .oauth2Login()
+                .loginPage("/login")
+                .userInfoEndpoint()
+                .userService(userService);
+//                .and()
+//                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .logoutSuccessUrl("/").permitAll();
     }
+
+    @Autowired
+    private CustomOAuth2UserService userService;
 }
+
+//    @Value("${nivio.loginType}")
+//    private String loginType;
+//
+//    if (loginType.equals("required"))
+//
+//    {
+//        @Override
+//        protected void configure (HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/**").permitAll()
+//                .and().oauth2Login().defaultSuccessUrl("/")
+//                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .logoutSuccessUrl("/")
+//                .invalidateHttpSession(true)
+//                .deleteCookies("JSESSIONID");
+//    }
+//    }
+
+
