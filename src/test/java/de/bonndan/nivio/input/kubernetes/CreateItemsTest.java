0cecb49e65fec37c4159a740821e9d4c629a9d2a
robert <robert@bitterling.de>
2022-01-10 17:24:55 +0100

    Fix K8s tests

--- src/test/java/de/bonndan/nivio/input/kubernetes/CreateItemsTest.java
+++ src/test/java/de/bonndan/nivio/input/kubernetes/CreateItemsTest.java
@@ -1,7 +1,9 @@
 package de.bonndan.nivio.input.kubernetes;
 
 import de.bonndan.nivio.input.ItemType;
-import de.bonndan.nivio.input.kubernetes.itemadapters.*;
+import de.bonndan.nivio.input.kubernetes.itemadapters.DeploymentItemAdapter;
+import de.bonndan.nivio.input.kubernetes.itemadapters.PersistentVolumeItemAdapter;
+import de.bonndan.nivio.input.kubernetes.itemadapters.PodItemAdapter;
 import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
 import io.fabric8.kubernetes.api.model.apps.ReplicaSetBuilder;
@@ -11,7 +13,6 @@ import io.fabric8.kubernetes.client.server.mock.EnableKubernetesMockClient;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
-import java.util.UUID;
 
 import static java.util.Map.entry;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -46,7 +47,7 @@ class CreateItemsTest {
     @Test
     void getPersistentVolumeClaimItems() {
         var persistentVolumeClaim = new PersistentVolumeClaimBuilder()
-                .withNewMetadata().withName("persistentvolumeclaim").withCreationTimestamp("testCreation").withUid("testUid").withLabels(Map.of("testLabelKey", "testLabelValue")).withOwnerReferences(new OwnerReferenceBuilder().withUid("testOwnerUid").build()).withNamespace("test").endMetadata()
+                .withNewMetadata().withCreationTimestamp("").withName("persistentvolumeclaim").withLabels(Map.of("testLabelKey", "testLabelValue")).withOwnerReferences(new OwnerReferenceBuilder().withUid("testOwnerUid").build()).withNamespace("test").endMetadata()
                 .withNewSpec().withStorageClassName("testStorageName").endSpec()
                 .withNewStatus().withNewPhase("testPhase").endStatus()
                 .build();
@@ -57,17 +58,17 @@ class CreateItemsTest {
         assertThat(persistentVolumeClaimList.get(0).getItemAdapter().getLabels()).containsExactly(entry("testLabelKey", "testLabelValue"));
         assertThat(persistentVolumeClaimList.get(0).getItemAdapter().getOwnerReferences().size()).isOne();
         assertThat(persistentVolumeClaimList.get(0).getItemAdapter().getOwnerReferences().get(0).getUid()).isEqualTo("testOwnerUid");
-        assertThat(persistentVolumeClaimList.get(0).getUid()).isEqualTo("testUid");
         assertThat(persistentVolumeClaimList.get(0).getClass()).isEqualTo(K8sItem.class);
-        assertThat(persistentVolumeClaimList.get(0).getDetails()).isEqualTo(Map.of("creation", "testCreation", "name", "persistentvolumeclaim", "namespace", "test", "phase status", "testPhase", "storage class", "testStorageName"));
-        assertThat(persistentVolumeClaimList.get(0).getItemAdapter()).isEqualToComparingFieldByField(new PersistentVolumeClaimItemAdapter(persistentVolumeClaim));
         assertThat(persistentVolumeClaimList.get(0).getType()).isEqualTo(ItemType.VOLUME);
+        var testDetails = persistentVolumeClaimList.get(0).getDetails();
+        testDetails.remove("creation");
+        assertThat(testDetails).isEqualTo(Map.of("name", "persistentvolumeclaim", "namespace", "test", "phase status", "testPhase", "storage class", "testStorageName"));
     }
 
     @Test
     void getPersistentVolumeItems() {
         var persistentVolume = new PersistentVolumeBuilder()
-                .withNewMetadata().withName("persistentvolume").withCreationTimestamp("testCreation").withUid("testUid").withLabels(Map.of("testLabelKey", "testLabelValue")).withOwnerReferences(new OwnerReferenceBuilder().withUid("testOwnerUid").build()).withNamespace("test").endMetadata()
+                .withNewMetadata().withName("persistentvolume").withLabels(Map.of("testLabelKey", "testLabelValue")).withOwnerReferences(new OwnerReferenceBuilder().withUid("testOwnerUid").build()).withNamespace("test").endMetadata()
                 .withNewSpec().withStorageClassName("testStorageName").withPersistentVolumeReclaimPolicy("testReclaimPolicy").addNewAccessMode("testAccessMode").addToCapacity("testCapacity", new Quantity("8", "Gi")).withClaimRef(new ObjectReferenceBuilder().withUid("testClaimUid").build()).endSpec()
                 .withNewStatus().withNewPhase("testPhase").endStatus()
                 .build();
@@ -78,17 +79,18 @@ class CreateItemsTest {
         assertThat(persistentVolumeList.get(0).getItemAdapter().getLabels()).containsExactly(entry("testLabelKey", "testLabelValue"));
         assertThat(persistentVolumeList.get(0).getItemAdapter().getOwnerReferences().size()).isOne();
         assertThat(persistentVolumeList.get(0).getItemAdapter().getOwnerReferences().get(0).getUid()).isEqualTo("testOwnerUid");
-        assertThat(persistentVolumeList.get(0).getUid()).isEqualTo("testUid");
         assertThat(persistentVolumeList.get(0).getClass()).isEqualTo(K8sItem.class);
-        assertThat(persistentVolumeList.get(0).getDetails()).isEqualTo(Map.of("creation", "testCreation", "name", "persistentvolume", "namespace", "test", "phase status", "testPhase", "reclaim policy", "testReclaimPolicy", "storage class", "testStorageName", "storage mode", "testAccessMode", "testCapacity", "8Gi"));
         assertThat(persistentVolumeList.get(0).getType()).isEqualTo(ItemType.VOLUME);
         assertThat(((PersistentVolumeItemAdapter) persistentVolumeList.get(0).getItemAdapter()).getClaimRef().getUid()).isEqualTo("testClaimUid");
+        var testDetails = persistentVolumeList.get(0).getDetails();
+        testDetails.remove("creation");
+        assertThat(testDetails).isEqualTo(Map.of("name", "persistentvolume", "namespace", "test", "phase status", "testPhase", "reclaim policy", "testReclaimPolicy", "storage class", "testStorageName", "storage mode", "testAccessMode", "testCapacity", "8Gi"));
     }
 
     @Test
     void getPodItems() {
         var pod = new PodBuilder()
-                .withNewMetadata().withName("pod").withCreationTimestamp("testCreation").withUid("testUid").withLabels(Map.of("testLabelKey", "testLabelValue")).withOwnerReferences(new OwnerReferenceBuilder().withUid("testOwnerUid").build()).withNamespace("test").endMetadata()
+                .withNewMetadata().withName("pod").withLabels(Map.of("testLabelKey", "testLabelValue")).withOwnerReferences(new OwnerReferenceBuilder().withUid("testOwnerUid").build()).withNamespace("test").endMetadata()
                 .withNewSpec().addNewVolume().withNewPersistentVolumeClaim().withNewClaimName("testClaimName").endPersistentVolumeClaim().endVolume().endSpec()
                 .withNewStatus().addNewCondition().withType("testType").withStatus("testStatus").endCondition().endStatus()
                 .build();
@@ -99,19 +101,19 @@ class CreateItemsTest {
         assertThat(podList.get(0).getItemAdapter().getLabels()).containsExactly(entry("testLabelKey", "testLabelValue"));
         assertThat(podList.get(0).getItemAdapter().getOwnerReferences().size()).isOne();
         assertThat(podList.get(0).getItemAdapter().getOwnerReferences().get(0).getUid()).isEqualTo("testOwnerUid");
-        assertThat(podList.get(0).getUid()).isEqualTo("testUid");
         assertThat(podList.get(0).getClass()).isEqualTo(K8sItem.class);
-        assertThat(podList.get(0).getDetails()).isEqualTo(Map.of("creation", "testCreation", InputFormatHandlerKubernetes.LABEL_PREFIX + ".boolcondition.testtype", "teststatus", "name", "pod", "namespace", "test"));
-        assertThat(podList.get(0).getItemAdapter()).isEqualToComparingFieldByField(new PodItemAdapter(pod));
         assertThat(((PodItemAdapter) podList.get(0).getItemAdapter()).getVolumes().size()).isOne();
         assertThat(((PodItemAdapter) podList.get(0).getItemAdapter()).getVolumes().get(0).getPersistentVolumeClaim().getClaimName()).isEqualTo("testClaimName");
         assertThat(podList.get(0).getType()).isEqualTo(ItemType.POD);
+        var testDetails = podList.get(0).getDetails();
+        testDetails.remove("creation");
+        assertThat(testDetails).isEqualTo(Map.of(InputFormatHandlerKubernetes.LABEL_PREFIX + ".boolcondition.testtype", "teststatus", "name", "pod", "namespace", "test"));
     }
 
     @Test
     void getReplicaSetItems() {
         var replicaSet = new ReplicaSetBuilder()
-                .withNewMetadata().withName("replicaSet").withCreationTimestamp("testCreation").withUid("testUid").withLabels(Map.of("testLabelKey", "testLabelValue")).withOwnerReferences(new OwnerReferenceBuilder().withUid("testOwnerUid").build()).withNamespace("test").endMetadata()
+                .withNewMetadata().withName("replicaSet").withLabels(Map.of("testLabelKey", "testLabelValue")).withOwnerReferences(new OwnerReferenceBuilder().withUid("testOwnerUid").build()).withNamespace("test").endMetadata()
                 .withNewSpec().withReplicas(1).endSpec()
                 .withNewStatus().addNewCondition().withType("testType").withStatus("testStatus").endCondition().withReadyReplicas(1).endStatus()
                 .build();
@@ -122,17 +124,17 @@ class CreateItemsTest {
         assertThat(replicaSetList.get(0).getItemAdapter().getLabels()).containsExactly(entry("testLabelKey", "testLabelValue"));
         assertThat(replicaSetList.get(0).getItemAdapter().getOwnerReferences().size()).isOne();
         assertThat(replicaSetList.get(0).getItemAdapter().getOwnerReferences().get(0).getUid()).isEqualTo("testOwnerUid");
-        assertThat(replicaSetList.get(0).getUid()).isEqualTo("testUid");
         assertThat(replicaSetList.get(0).getClass()).isEqualTo(K8sItem.class);
-        assertThat(replicaSetList.get(0).getDetails()).isEqualTo(Map.of("creation", "testCreation", InputFormatHandlerKubernetes.LABEL_PREFIX + ".replicacondition.replicas", "1;1", "name", "replicaSet", "namespace", "test"));
-        assertThat(replicaSetList.get(0).getItemAdapter()).isEqualToComparingFieldByField(new ReplicaSetItemAdapter(replicaSet));
         assertThat(replicaSetList.get(0).getType()).isEqualTo(ItemType.REPLICASET);
+        var testDetails = replicaSetList.get(0).getDetails();
+        testDetails.remove("creation");
+        assertThat(testDetails).isEqualTo(Map.of(InputFormatHandlerKubernetes.LABEL_PREFIX + ".replicacondition.replicas", "1;1", "name", "replicaSet", "namespace", "test"));
     }
 
     @Test
     void getServiceItems() {
         var service = new ServiceBuilder()
-                .withNewMetadata().withName("service").withCreationTimestamp("testCreation").withUid("testUid").withLabels(Map.of("testLabelKey", "testLabelValue")).withOwnerReferences(new OwnerReferenceBuilder().withUid("testOwnerUid").build()).withNamespace("test").endMetadata()
+                .withNewMetadata().withName("service").withLabels(Map.of("testLabelKey", "testLabelValue")).withOwnerReferences(new OwnerReferenceBuilder().withUid("testOwnerUid").build()).withNamespace("test").endMetadata()
                 .withNewSpec().withClusterIP("testIP").withNewType("testType").withSessionAffinity("testSessionAffinity").endSpec()
                 .build();
         kubernetesClient.services().create(service);
@@ -142,17 +144,18 @@ class CreateItemsTest {
         assertThat(serviceList.get(0).getItemAdapter().getLabels()).containsExactly(entry("testLabelKey", "testLabelValue"));
         assertThat(serviceList.get(0).getItemAdapter().getOwnerReferences().size()).isOne();
         assertThat(serviceList.get(0).getItemAdapter().getOwnerReferences().get(0).getUid()).isEqualTo("testOwnerUid");
-        assertThat(serviceList.get(0).getUid()).isEqualTo("testUid");
         assertThat(serviceList.get(0).getClass()).isEqualTo(K8sItem.class);
-        assertThat(serviceList.get(0).getDetails()).isEqualTo(Map.of("cluster ip", "testIP", "creation", "testCreation", "name", "service", "namespace", "test", "service type", "testType", "session affinity", "testSessionAffinity"));
-        assertThat(serviceList.get(0).getItemAdapter()).isEqualToComparingFieldByField(new ServiceItemAdapter(service));
+        ;
         assertThat(serviceList.get(0).getType()).isEqualTo(ItemType.SERVICE);
+        var testDetails = serviceList.get(0).getDetails();
+        testDetails.remove("creation");
+        assertThat(testDetails).isEqualTo(Map.of("cluster ip", "testIP", "name", "service", "namespace", "test", "service type", "testType", "session affinity", "testSessionAffinity"));
     }
 
     @Test
     void getStatefulSetItems() {
         var statefulSet = new StatefulSetBuilder()
-                .withNewMetadata().withName("statefulSet").withCreationTimestamp("testCreation").withUid("testUid").withLabels(Map.of("testLabelKey", "testLabelValue")).withOwnerReferences(new OwnerReferenceBuilder().withUid("testOwnerUid").build()).withNamespace("test").endMetadata()
+                .withNewMetadata().withName("statefulSet").withLabels(Map.of("testLabelKey", "testLabelValue")).withOwnerReferences(new OwnerReferenceBuilder().withUid("testOwnerUid").build()).withNamespace("test").endMetadata()
                 .withNewSpec().withReplicas(1).endSpec()
                 .withNewStatus().addNewCondition().withType("testType").withStatus("testStatus").endCondition().withReadyReplicas(1).endStatus()
                 .build();
@@ -163,10 +166,10 @@ class CreateItemsTest {
         assertThat(statefulSetList.get(0).getItemAdapter().getLabels()).containsExactly(entry("testLabelKey", "testLabelValue"));
         assertThat(statefulSetList.get(0).getItemAdapter().getOwnerReferences().size()).isOne();
         assertThat(statefulSetList.get(0).getItemAdapter().getOwnerReferences().get(0).getUid()).isEqualTo("testOwnerUid");
-        assertThat(statefulSetList.get(0).getUid()).isEqualTo("testUid");
         assertThat(statefulSetList.get(0).getClass()).isEqualTo(K8sItem.class);
-        assertThat(statefulSetList.get(0).getDetails()).isEqualTo(Map.of("creation", "testCreation", InputFormatHandlerKubernetes.LABEL_PREFIX + ".replicacondition.replicas", "1;1", "name", "statefulSet", "namespace", "test"));
-        assertThat(statefulSetList.get(0).getItemAdapter()).isEqualToComparingFieldByField(new StatefulSetItemAdapter(statefulSet));
         assertThat(statefulSetList.get(0).getType()).isEqualTo(ItemType.STATEFULSET);
+        var testDetails = statefulSetList.get(0).getDetails();
+        testDetails.remove("creation");
+        assertThat(testDetails).isEqualTo(Map.of(InputFormatHandlerKubernetes.LABEL_PREFIX + ".replicacondition.replicas", "1;1", "name", "statefulSet", "namespace", "test"));
     }
 }
