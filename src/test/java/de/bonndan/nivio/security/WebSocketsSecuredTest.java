7c63822e99727e5a85c6d5666ceb110c2858d25e
Daniel Pozzi <daniel@dedica.team>
2022-01-11 17:25:53 +0100

    [#744] windows test fix attempt

--- src/test/java/de/bonndan/nivio/security/WebSocketsSecuredTest.java
+++ src/test/java/de/bonndan/nivio/security/WebSocketsSecuredTest.java
@@ -1,10 +1,11 @@
 package de.bonndan.nivio.security;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.env.Environment;
 import org.springframework.messaging.converter.StringMessageConverter;
-import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
@@ -13,34 +14,37 @@ import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 import javax.websocket.DeploymentException;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
 
 import static de.bonndan.nivio.notification.WebSocketConfig.SUBSCRIBE;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@SpringBootTest(properties = {"nivio.loginMode=required"},webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(properties = {"nivio.loginMode=required"}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebSocketsSecuredTest {
 
-    @LocalServerPort
-    private Integer port;
+    Integer port = 0;
+
+    @Autowired
+    Environment environment;
+
+    @BeforeEach
+    void setup() {
+        port = Integer.valueOf(environment.getProperty("local.server.port"));
+    }
 
 
     /**
      * https://rieckpil.de/write-integration-tests-for-your-spring-websocket-endpoints/
      */
     @Test
-    void securedSockets() throws InterruptedException, ExecutionException, TimeoutException {
-        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue(1);
+    void securedSockets() {
 
         var webSocketStompClient = new WebSocketStompClient(new SockJsClient(List.of(new WebSocketTransport(new StandardWebSocketClient()))));
         webSocketStompClient.setMessageConverter(new StringMessageConverter());
 
         assertThatThrownBy(() -> {
-            StompSession session = webSocketStompClient.connect("ws://localhost:" + port + "/" + SUBSCRIBE,
+            webSocketStompClient.connect("ws://localhost:" + port + "/" + SUBSCRIBE,
                             new StompSessionHandlerAdapter() {
                             })
                     .get(1, SECONDS);
