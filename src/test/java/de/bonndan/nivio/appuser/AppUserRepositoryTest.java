05d9085bf67384ce130044cba7c47c15b29b3061
Jennifer Arps <jennifer@dedica.team>
2022-01-21 15:20:05 +0100

    [#141] add integration repository test

---
+++ src/test/java/de/bonndan/nivio/appuser/AppUserRepositoryTest.java
@@ -0,0 +1,42 @@
+package de.bonndan.nivio.appuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+class AppUserRepositoryTest {
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Test
+    void findByExternalId() {
+        Optional<AppUser> appUser = Optional.of(new AppUser());
+        appUser.get().setExternalId("100");
+        appUser.get().setAlias("login");
+        appUser.get().setAppUserRole(AppUserRole.USER);
+        appUser.get().setId(1L);
+        appUser.get().setIdp("github");
+
+        appUser.ifPresent(user -> appUserRepository.save(user));
+        final Optional<AppUser> fetchedAppUser = appUserRepository.findByExternalId(appUser.get().getExternalId());
+
+        assertNotNull(appUser);
+
+        assertThat(fetchedAppUser)
+                .hasValueSatisfying(fetched -> {
+                    assertThat(fetched.getExternalId()).isNotNull().isEqualTo(appUser.get().getExternalId());
+                    assertThat(fetched.getAlias()).isNotNull().isEqualTo(appUser.get().getAlias());
+                    assertThat(fetched.getId()).isNotNull().isEqualTo(appUser.get().getId());
+                    assertThat(fetched.getIdp()).isNotNull().isEqualTo(appUser.get().getIdp());
+                    assertThat(fetched.getAppUserRole()).isNotNull().isEqualTo(appUser.get().getAppUserRole());
+
+                });
+    }
+}
