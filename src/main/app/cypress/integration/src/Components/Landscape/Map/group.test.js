69e32da36b7600e8ac52efd28ce125a5b91fa66c
robert <robert@bitterling.de>
2022-01-24 14:45:36 +0100

    Change tests for heroku

--- src/main/app/cypress/integration/src/Components/Landscape/Map/group.test.js
+++ src/main/app/cypress/integration/src/Components/Landscape/Map/group.test.js
@@ -1,35 +1,21 @@
 describe('Group test', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000');
+    cy.visit('');
   });
   it('Visits Nivio and checks the groups', () => {
     cy.wait(1000);
-    cy.get('[href^="#/landscape/"]').each(($el, index, $list) => {
-      cy.wrap($el)
-        .invoke('attr', 'href')
-        .then((href) => {
-          cy.visit(href);
-          cy.url().should('include', href);
-        });
       cy.get('.ReactSVGPanZoom > svg:nth-child(1)').click();
       cy.get('[style="position: absolute; right: 0.5em;"] > .MuiButtonBase-root').click();
 
       cy.get('[class="groupArea unselected"]').each(($el, index, $list) => {
-        cy.get('[class^="makeStyles-sideBar-"]').should('not.be.visible');
+        cy.get('[class^="MuiPaper-root MuiCard-root "]').should('not.exist');
         cy.wrap($el).click({ force: true });
         console.log($el.find('[class="groupLabel"]').text());
-        cy.get('[class^="makeStyles-sideBar-"]').should('be.visible');
+        cy.get('[class^="MuiPaper-root MuiCard-root "]').should('be.visible');
         cy.get('.MuiCardHeader-root').should('contain.text', $el.find('[class="label"]').text(), {
           matchCase: false,
         });
         cy.get('.MuiCardHeader-action > .MuiButtonBase-root').click();
       });
-      cy.get('.MuiList-root > [tabindex="0"]')
-        .invoke('attr', 'href')
-        .then((href) => {
-          cy.visit(href);
-        });
-      cy.wait(400);
-    });
   });
 });
