f6636f4b38e1a2417db3fdc68c4a6f96efbeda43
robert <robert@bitterling.de>
2022-01-24 12:05:35 +0100

    Improved tests and add GithHub actions config

---
+++ src/main/app/cypress/integration/src/Components/Landscape/Map/group_test.spec.js
@@ -0,0 +1,35 @@
+describe('Group test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+  it('Visits Nivio and checks the groups', () => {
+    cy.wait(1000);
+    cy.get('[href^="#/landscape/"]').each(($el, index, $list) => {
+      cy.wrap($el)
+        .invoke('attr', 'href')
+        .then((href) => {
+          cy.visit(href);
+          cy.url().should('include', href);
+        });
+      cy.get('.ReactSVGPanZoom > svg:nth-child(1)').click();
+      cy.get('[style="position: absolute; right: 0.5em;"] > .MuiButtonBase-root').click();
+
+      cy.get('[class="groupArea unselected"]').each(($el, index, $list) => {
+        cy.get('[class^="makeStyles-sideBar-"]').should('not.be.visible');
+        cy.wrap($el).click({ force: true });
+        console.log($el.find('[class="groupLabel"]').text());
+        cy.get('[class^="makeStyles-sideBar-"]').should('be.visible');
+        cy.get('.MuiCardHeader-root').should('contain.text', $el.find('[class="label"]').text(), {
+          matchCase: false,
+        });
+        cy.get('.MuiCardHeader-action > .MuiButtonBase-root').click();
+      });
+      cy.get('.MuiList-root > [tabindex="0"]')
+        .invoke('attr', 'href')
+        .then((href) => {
+          cy.visit(href);
+        });
+      cy.wait(400);
+    });
+  });
+});
--- src/main/app/cypress/integration/src/Components/Navigation/navigation_test.js
+++ src/main/app/cypress/integration/src/Components/Navigation/navigation_test.js
@@ -1,21 +1,39 @@
 describe('Navigation test', () => {
-  it('Visits Nivio and checks the navigation bar', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000');
+  });
+  it('Visits Nivio and checks the navigation bar', () => {
     cy.url().should('include', '/#/');
+    cy.wait(1000);
+    cy.get('[href^="#/landscape/"]').first().click();
+    cy.url().should('include', '/landscape/');
+    cy.get('.MuiAvatar-img').click();
+    cy.get('.MuiList-root > [tabindex="0"]').click();
+    cy.url().should('match', new RegExp('/#/$'));
     cy.wait(400);
+    cy.get('.MuiAvatar-img').click();
+    cy.get('[href="#/man/install.html"]').click();
+    cy.url().should('include', '/#/man/install.html');
+    cy.get('.MuiAvatar-img').click();
+    cy.get('.MuiList-root > [tabindex="0"]').click();
+  });
 
+  it('Visits Nivio and checks if all landscapes are reachable', () => {
+    cy.wait(1000);
     cy.get('[href^="#/landscape/"]').each(($el, index, $list) => {
-      cy.wrap($el).click();
-      cy.url().should('include', '/landscape/');
-      cy.get('.MuiAvatar-img').click();
-      cy.get('.MuiList-root > [tabindex="0"]').click();
-      cy.url().should('match', new RegExp('/#/$'));
+      cy.wrap($el)
+        .invoke('attr', 'href')
+        .then((href) => {
+          cy.visit(href);
+          cy.url().should('include', href);
+        });
+      cy.get('.MuiList-root > [tabindex="0"]')
+        .invoke('attr', 'href')
+        .then((href) => {
+          cy.visit(href);
+          cy.url().should('match', new RegExp('/#/$'));
+        });
       cy.wait(400);
-      cy.get('.MuiAvatar-img').click();
-      cy.get('[href="#/man/install.html"]').click();
-      cy.url().should('include', '/#/man/install.html');
-      cy.get('.MuiAvatar-img').click();
-      cy.get('.MuiList-root > [tabindex="0"]').click();
     });
   });
 });
