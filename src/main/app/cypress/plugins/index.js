335058f12b9c19037591ca62bbe8b0a1ff584069
robert <robert@bitterling.de>
2022-01-25 08:53:16 +0100

    Change GitHubAction config

--- src/main/app/cypress/plugins/index.js
+++ src/main/app/cypress/plugins/index.js
@@ -1,23 +1,7 @@
-/// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-/**
- * @type {Cypress.PluginConfig}
- */
-const injectDevServer = require("@cypress/react/plugins/react-scripts")
-// eslint-disable-next-line no-unused-vars
+// export a function
 module.exports = (on, config) => {
-  injectDevServer(on, config)
-  return config
+  // bind to the event we care about
+  on('<event>', (arg1, arg2) => {
+    // plugin stuff here
+  });
 }
